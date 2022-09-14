82661157645c5ee5ced8ede34448e226bfe39b54
Juliane <julianesilvac75@gmail.com>
2022-09-14 23:58:04 +0100

    feat: implements interface for cars

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number().int().positive().gte(2)
+    .lte(4),
+  seatsQty: z.number().int().positive().gte(2)
+    .lte(7),
+});
+
+type ICar = z.infer<typeof CarZodSchema>;
+
+export {
+  CarZodSchema,
+  ICar,
+};
