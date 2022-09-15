e2f5ab254cc8bd86e7724cce8903d72c99e6dc96
Juliane <julianesilvac75@gmail.com>
2022-09-15 23:49:39 +0100

    feat: implements GET /cars route

--- src/routes/carRouter.ts
+++ src/routes/carRouter.ts
@@ -10,5 +10,6 @@ const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
 carRouter.post('/cars', (req, res) => carController.create(req, res));
+carRouter.get('/cars', (req, res) => carController.read(req, res));
 
 export default carRouter;
