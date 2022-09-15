fc0e97bb745aab0039531926a74823405fb155b7
Juliane <julianesilvac75@gmail.com>
2022-09-15 21:09:57 +0100

    feat: implements all methods on the model layer of cars

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,4 +1,4 @@
-import { Model } from 'mongoose';
+import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
 import { IModel } from '../interfaces/IModel';
 
 abstract class MongoModel<T> implements IModel<T> {
@@ -12,10 +12,31 @@ abstract class MongoModel<T> implements IModel<T> {
     return this._model.create({ ...obj });
   }
   
-  // read(): Promise<T[]>,
-  // readOne(_id: string): Promise<T | null>,
-  // update(_id: string, obj: T): Promise<T | null>,
-  // delete(_id: string): Promise<T | null>
+  public async read(): Promise<T[]> {
+    return this._model.find({});
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('Erro de id invalid');
+    
+    return this._model.findOne({ _id });
+  }
+
+  public async update(_id: string, obj: T): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('Erro de id invalido');
+
+    return this._model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<T>,
+      // { new: true },
+    );
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('Erro de id invalido');
+    
+    return this._model.findByIdAndRemove({ _id });
+  }
 }
 
 export default MongoModel;
