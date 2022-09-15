c4593910b660a6ece9299ab9a704b291ceb8f747
Juliane <julianesilvac75@gmail.com>
2022-09-16 00:25:30 +0100

    test: readOne method on controller layer of cars

--- src/tests/unit/controllers/car.test.ts
+++ src/tests/unit/controllers/car.test.ts
@@ -67,4 +67,28 @@ describe('Car Controller', () => {
       expect((res.json as sinon.SinonStub).calledWith([carMockWithId])).to.be.true;
     });
   });
+
+  describe('When trying to get one car by the id,', () => {
+    beforeEach(() => {
+      sinon.stub(carService, 'readOne').resolves(carMockWithId);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it ('should return status 200', async () => {
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(StatusCodes.OK)).to.be.true;
+    });
+
+    it('should return a body with the car', async () => {
+      await carController.readOne(req, res);
+
+      expect((res.json as sinon.SinonStub).calledWith(carMockWithId)).to.be.true;
+    });
+  });
 });
